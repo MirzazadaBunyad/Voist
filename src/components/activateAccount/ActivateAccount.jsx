21bf153416f7e428f99ed7670118b03709574c62
Mirzazada Bunyad <115315853+MirzazadaBunyad@users.noreply.github.com>
2024-05-21 17:53:22 +0400

    new

--- src/components/activateAccount/ActivateAccount.jsx
+++ src/components/activateAccount/ActivateAccount.jsx
@@ -1,75 +1,7 @@
-import { useRef, useState } from "react";
 import styles from "./activateAccount.module.scss";
 import checkWhiteIcon from "../../assets/img/checkWhiteIcon.svg";
-import arrowRightBlack from "../../assets/img/arrowRightBlack.svg";
 
-function ActivateAccount({ data, backToLogin, goToLogin }) {
-  const [inputs, setInputs] = useState(["", "", "", "", "", ""]);
-
-  const handleSubmit = async (e) => {
-    e && e.preventDefault();
-    const combinedInputs = inputs.join("");
-    backToLogin();
-    goToLogin();
-
-    try {
-      const response = await fetch(
-        `https://safaraliyev.live/api/user/verify/${combinedInputs}/`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error: Status ${response.status}`);
-      }
-
-      console.log("Data sent successfully!");
-      // setActivationCodeSent(true);
-      // setShowActivation(true);
-    } catch (error) {
-      console.error("Error sending data:", error.message);
-      // Add error handling logic here
-    }
-  };
-  const input1 = useRef(null),
-    input2 = useRef(null),
-    input3 = useRef(null),
-    input4 = useRef(null),
-    input5 = useRef(null),
-    input6 = useRef(null);
-
-  function handleInput(event, index) {
-    const value = event.target.value;
-    const maxLength = parseInt(event.target.getAttribute("maxlength"));
-
-    if (value.length <= maxLength) {
-      setInputs((prevInputs) => {
-        const newInputs = [...prevInputs];
-        newInputs[index] = value;
-        return newInputs;
-      });
-
-      if (index < 5) {
-        if (value.length >= maxLength) {
-          if (index === 0) {
-            input2.current.focus();
-          } else if (index === 1) {
-            input3.current.focus();
-          } else if (index === 2) {
-            input4.current.focus();
-          } else if (index === 3) {
-            input5.current.focus();
-          } else if (index === 4) {
-            input6.current.focus();
-          }
-        }
-      }
-    }
-  }
+function ActivateAccount({ data }) {
   return (
     <div className={styles.activation}>
       <div className={styles.checkIcon}>
@@ -80,41 +12,6 @@ function ActivateAccount({ data, backToLogin, goToLogin }) {
         Check your email <span>({data.email})</span> to activate your recruitee
         account
       </p>
-      <form className={styles.formContainer} onSubmit={handleSubmit}>
-        <div className={styles.inputContainer}>
-          {inputs.map((value, index) => (
-            <input
-              key={index}
-              className={styles.LoginInput}
-              type="number"
-              id="belowInput"
-              name="number"
-              placeholder="•"
-              ref={
-                index === 0
-                  ? input1
-                  : index === 1
-                  ? input2
-                  : index === 2
-                  ? input3
-                  : index === 3
-                  ? input4
-                  : index === 4
-                  ? input5
-                  : input6
-              }
-              maxLength={1}
-              value={value}
-              onChange={(e) => handleInput(e, index)}
-            />
-          ))}
-        </div>
-        <div className={styles.buttonElement}>
-          <button type="submit" className={styles.sendButton}>
-            Confirm Verification <img src={arrowRightBlack} alt="Error" />
-          </button>
-        </div>
-      </form>
       <div className={styles.sendAgain}>
         <p>Didn’t receive e-mail?</p>
         <button className={styles.sendAgainButton}>Send again</button>
